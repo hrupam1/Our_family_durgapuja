0c9b062aad4c7a1a6c66aee25f29c382176df4f5
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-09-17 01:18:44 +0530

    Add site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
             <head>
+                <meta name="8c93adff43e25dfc4d5d1c9fa6f38282df878205" content="8c93adff43e25dfc4d5d1c9fa6f38282df878205" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4628453462795407" crossOrigin="anonymous"></script>
             </head>
             <body className="font-sans antialiased text-puja-ivory bg-puja-dark selection:bg-puja-gold selection:text-puja-dark flex flex-col min-h-screen">
