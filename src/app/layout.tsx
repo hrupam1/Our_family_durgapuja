3328fc19971ac6c3dfa8b1be1a0524c3076204a7
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-09-23 03:47:30 +0530

    Add Chatzy widget script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,8 @@ export default function RootLayout({
             <head>
                 <meta name="8c93adff43e25dfc4d5d1c9fa6f38282df878205" content="8c93adff43e25dfc4d5d1c9fa6f38282df878205" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4628453462795407" crossOrigin="anonymous"></script>
+                <link rel="stylesheet" href="https://chatzy-kb-store.s3.amazonaws.com/icons/5ab07987-b5db-477c-82ff-1287e0883acb"/>
+                <script src="https://chatzy-kb-store.s3.amazonaws.com/icons/56706cc4-b3ba-4eba-9610-f2fb07008a5c" id="93b8764b-a4d2-4683-9c39-fdb2ed251292" className="chatzy_widget_script" defer></script>
             </head>
             <body className="font-sans antialiased text-puja-ivory bg-puja-dark selection:bg-puja-gold selection:text-puja-dark flex flex-col min-h-screen">
                 <SmoothScrolling>
